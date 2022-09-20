99217c3a967268c1d402d420c00730278d2ada02
Chunyue Ma <cma01@wesleyan.edu>
2022-09-19 22:15:50 -0700

    add dimension reduction code for java solution

--- q0198/q0198.java
+++ q0198/q0198.java
@@ -15,4 +15,24 @@ class Solution {
         }
         return maxMoney[n - 1];
     }
+
+    public int robDimensionReduction(int[] nums) {
+        int n = nums.length;
+        if (n == 1)
+            return nums[0];
+        else if (n == 2)
+            return Math.max(nums[0], nums[1]);
+
+        int ans1 = nums[0];
+        int ans2 = Math.max(nums[0], nums[1]);
+        int res = 0;
+
+        for (int i = 2; i < n; i++) {
+            res = Math.max(ans1 + nums[i], ans2);
+            ans1 = ans2;
+            ans2 = res;
+        }
+
+        return res;
+    }
 }
